f8a9c626d3dce05000dc98163da89cae184b0cee
brandon <bbroni1@live.spcollege.edu>
2015-12-03 17:26:34 -0500

    init autoload

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -9,28 +9,11 @@
 |
 */
 
-require_once dirname(__FILE__) . '../../vendor/autoload.php';
+if (!file_exists(dirname(__FILE__) . '/../vendor/autoload.php')) {
+	echo "Please install Composer and run <b>composer install</b> in the working directory.";
+	exit();
+}
 
-
-
-//config array
-$config = array();
-
-//setup main app
-$config['app'] = array(
-	'name' => 'SSE'
-);
-
-//setup database settings
-$config['db'] = array(
-    'driver'   => 'mysql',
-    'host'     => 'localhost',
-    'port'     => '3306',
-    'user'     => 'root',
-    'password' => 'bbbb1111',
-    'name'     => 'db_name',
-);
-
-$DB = new DB;
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
 
 ?>
