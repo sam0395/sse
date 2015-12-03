84100a2287c40b153828637541efafd0fe1b08ac
brandon <bbroni1@live.spcollege.edu>
2015-12-03 17:27:21 -0500

    init index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,25 @@
 <?php
 
 /*
-* Autoload all the classes
+|------------------------------------------------------------
+| Register The Composer Auto Loader
+|------------------------------------------------------------
+|
+| Composer provides an autoloader that we can use to load all of our classes
+|
 */
 
 require_once dirname(__FILE__) . '/../bootstrap/autoload.php';
+
+/*
+|------------------------------------------------------------
+| Register The Application
+|------------------------------------------------------------
+|
+| This will initialize the application
+|
+*/
+
+$app = new sse\App\App;
 
 ?>
