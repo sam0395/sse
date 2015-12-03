5f8ba2935ba2ce2fa0247722fe489f72c24ef051
brandon <bbroni1@live.spcollege.edu>
2015-12-02 22:39:16 -0500

    init bootstrap

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,4 +1,23 @@
 <?php
 // Including global autoloader
 require_once dirname(__FILE__) . '../../vendor/autoload.php';
+
+//config array
+$config = array();
+
+//setup main app
+$config['app'] = array(
+	'name' => 'SSE'
+);
+
+//setup database settings
+$config['db'] = array(
+    'driver'   => 'mysql',
+    'host'     => 'localhost',
+    'port'     => '3306',
+    'user'     => 'root',
+    'password' => 'bbbb1111',
+    'name'     => 'db_name',
+);
+
 ?>
