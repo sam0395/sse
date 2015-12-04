37a1499f9277873eeb07c820ee6847e4bca59219
brandon <bbroni1@live.spcollege.edu>
2015-12-04 15:51:47 -0500

    Rename src/

---
+++ src/app.php
@@ -0,0 +1,71 @@
+<?php 
+namespace sse\App;
+
+/*
+|------------------------------------------------------------
+| Application Instance
+|------------------------------------------------------------
+|
+| Initialize a new instance of app to construct the application
+|
+*/
+
+class App
+{
+
+	/**
+	* The SSE framework version
+	*
+	* @var string
+	*/
+	const VERSION = "1.0";
+
+	/**
+	* The name of the application
+	*
+	* @var string
+	*/
+	protected $name;
+
+	/**
+     *	Setup variables
+     *
+     * @return void
+     */
+	public function __construct($name)
+	{
+		self::setName($name);
+	}
+
+	/**
+	* Get the application version
+	*
+	* @var string
+	*/
+	public function getVersion()
+	{
+		return static::VERSION;
+	}
+
+	/**
+	* Get the application name
+	*
+	* @var string
+	*/
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	* Set the application name
+	*
+	* @var string
+	*/
+	public function setName($name)
+	{
+		return $this->name = $name;
+	}		
+}
+
+?>
