9f0d9cbb4a28fc8fe18b61f82630753ebcb85736
brandon <bbroni1@live.spcollege.edu>
2015-12-05 15:43:07 -0500

    init route.php

---
+++ src/router/route.php
@@ -0,0 +1,21 @@
+<?php 
+namespace Trace\App;
+
+/*
+|------------------------------------------------------------
+| Route
+|------------------------------------------------------------
+|
+| Initialize a new instance of app to construct the application
+|
+*/
+
+class Route
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
+?>
