91bf471f25aec4f0c6503f066ae5ff5aa126e7a1
brandon <bbroni1@live.spcollege.edu>
2015-12-09 16:47:46 -0500

    Modify Register.php

--- src/Register.php
+++ src/Register.php
@@ -3,18 +3,41 @@ namespace Trace\Core;
 
 class Register
 {
+
+	/**
+	* Holds all of the class instances
+	*
+	* @var array
+	*/
 	private static $instances = array();
 
+	/**
+	* Prevent construct
+	*
+	* @return void
+	*/
 	private function __construct()
 	{
-		// Prevent Construct
+
 	}
 
+	/**
+	* Prevent clone
+	*
+	* @return void
+	*/
 	private function __clone()
 	{
-		// Prevent Clone
+
 	}
 
+	/**
+	* Get instance of class
+	*
+	* @param string $key
+	* @param string [optional] $default
+	* @return $default
+	*/
 	public static function getInstance($key, $default = null)
 	{
 		if (isset(self::$instances[$key])) {
@@ -24,14 +47,26 @@ class Register
 		return $default;
 	}
 
+	/**
+	* Get instance
+	*
+	* @param string $key
+	* @param object [optional] $instance
+	* @return void
+	*/
 	public static function setInstance($key, $instance = null)
 	{
 		self::$instances[$key] = $instance;
 	}
 
-	public static function erase($key)
+	/**
+	* Destroy instance
+	*
+	* @param string $key
+	* @return void
+	*/
+	public static function destroy($key)
 	{
 		unset(self::$instances[$key]);
 	}
 }
-?>
